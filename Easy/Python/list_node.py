e089e56350c49654ff72d03cce0d7398a54d908b
enzhong-wang <enzhong.wang.jr@gmail.com>
2024-06-30 19:33:29 -0400

    move list_node to common libs

--- Easy/Python/list_node.py
+++
@@ -1,8 +0,0 @@
-class ListNode:
-   def __init__(self, data: int=0):
-      self.data: int = data
-      self.next: ListNode = None
-
-   def add_next(self, node: 'ListNode'):
-      self.next = node
-
