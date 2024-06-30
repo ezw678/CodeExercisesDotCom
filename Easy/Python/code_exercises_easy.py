ceb7992d2634ebd3f794266d85e7aae6281499b5
enzhong-wang <enzhong.wang.jr@gmail.com>
2024-06-30 18:09:20 -0400

    add code exercise easy

---
+++ Easy/Python/code_exercises_easy.py
@@ -0,0 +1,139 @@
+from tree_node import TreeNode
+from list_node import ListNode
+import numpy as np
+
+
+# Write a method that sums two numbers.
+
+
+def sum_a_b(i: int, j: int) -> int:
+    return i + j
+
+
+"""Given a number n, write a method that sums all multiples of three and five up to
+n (inclusive)."""
+
+
+def sum_35_multiples(n: int) -> int:
+    return sum(n for n in range(3, n + 1) if n % 3 == 0 or n % 5 == 0)
+
+
+"""Write a method that calculates the factorial of a given number.
+Factorial is the product of all positive integers less than or equal
+to n. For example, factorial(4) = 4x3x2x1 = 24.
+TIP: To make it more interesting, try to do it recursively."""
+
+
+def factorial(n: int) -> int:
+    if n == 1:
+        return 1
+    return factorial(n - 1) * n
+
+
+"""Write a method that returns the index of the first occurrence of given integer in a list.
+Assume that the index of the first element in the list is zero.
+If the number does not exist return -1."""
+
+
+def search(n: int, ls: list[int]) -> int:
+    return ls.index(n) if n in ls else -1
+
+
+"""Write a method that reverses a string.
+For example, 'java interview' becomes 'weivretni avaj'."""
+
+
+def reverse_str(s: str) -> str:
+    return s[::-1]
+
+
+"""Write a method that returns the largest integer in the list.
+You can assume that the list has at least one element."""
+
+
+def maximum(nums: list[int]) -> int:
+    return max(nums)
+
+
+# Write a method that returns the average of a list of integers.
+def average_1(nums: list[int]) -> float:
+    return sum(nums) / len(nums)
+
+
+def average_2(nums: list[int]) -> float:
+    return np.average(nums)
+
+
+def average_3(nums: list[int]) -> float:
+    return np.mean(nums)
+
+
+# Write a method that converts all strings in a list to their upper case.
+def upper_case(strs: list[str]) -> list[str]:
+    return [s.upper() for s in strs]
+
+
+"""Write a method that returns the nth odd element of a list. If the index of the 
+element exceeds the list size, then return -1."""
+
+
+def get_element(nums: list[int], n: int) -> int:
+    idx = 2 * n - 2
+    return nums[idx] if idx < len(nums) else -1
+
+
+"""Write a method that returns the number of nodes in a binary tree.
+TreeNode API methods: node.left() and node.right()."""
+
+
+def get_number_of_nodes(node: TreeNode) -> int:
+    if node == None:
+        return 0
+
+    return 1 + get_number_of_nodes(node.left) + get_number_of_nodes(node.right)
+
+
+def count(node: ListNode) -> int:
+    if node == None:
+        return 0
+
+    return 1 + count(node.next)
+
+
+# Count the number of leaf nodes in a binary tree
+def count_leaf_nodes(node: TreeNode) -> TreeNode:
+    if node == None:
+        return 0
+
+    if node.left == None and node.right == None:
+        return 1
+
+    return count_leaf_nodes(node.left) + count_leaf_nodes(node.right)
+
+
+"""Write a method that returns the maximum depth of a binary tree. The maximum depth
+is the number of nodes from the root (inclusive) until the furthest node (inclusive)."""
+
+
+def depth_binary_tree(node: TreeNode) -> int:
+    if node == None:
+        return 0
+
+    return 1 + max(depth_binary_tree(node.left), depth_binary_tree(node.right))
+
+
+"""Find the second largest number in an integer array. You can assume a minimum array 
+length of two."""
+
+
+def find(nums: list[int]) -> int:
+    nums.sort(reverse=True)
+    return nums[1]
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ Easy/Python/test_easy.py
@@ -0,0 +1,149 @@
+from CodeExercisesDCEasy import *
+from tree_node import TreeNode
+
+
+def test_sum_a_b():
+    assert sum_a_b(1, 2) == 3
+
+
+def test_sum_35_multiples():
+    assert sum_35_multiples(15) == 60
+    assert sum_35_multiples(10) == 33
+    assert sum_35_multiples(110) == 2843
+
+
+def test_factorial():
+    assert factorial(5) == 120
+    assert factorial(1) == 1
+    assert factorial(4) == 24
+    assert factorial(6) == 720
+    assert factorial(8) == 40320
+
+
+def test_search():
+    assert search(6, [3, 1, 3, 2, 6, 9, 7, 6]) == 4
+    assert search(10, [10]) == 0
+    assert search(10, [103, -110]) == -1
+    assert search(22, [22, 22, 22, 22]) == 0
+
+
+def test_reverse_str():
+    assert reverse_str("java interview") == "weivretni avaj"
+    assert reverse_str("aba") == "aba"
+    assert reverse_str("code exercise") == "esicrexe edoc"
+    assert reverse_str("yes") == "sey"
+
+
+def test_average_1():
+    assert average_1([0, 1]) == 0.5
+    assert average_1([100, 10]) == 55.0
+    assert average_1([20, 20, 20]) == 20.0
+    assert average_1([2]) == 2.0
+
+
+def test_average_2():
+    assert average_2([0, 1]) == 0.5
+    assert average_2([100, 10]) == 55.0
+    assert average_2([20, 20, 20]) == 20.0
+    assert average_2([2]) == 2.0
+
+
+def test_average_3():
+    assert average_3([0, 1]) == 0.5
+    assert average_3([100, 10]) == 55.0
+    assert average_3([20, 20, 20]) == 20.0
+    assert average_3([2]) == 2.0
+
+
+def test_upperCase():
+    assert upper_case(["abc", "bbb"]) == ["ABC", "BBB"]
+    assert upper_case(["ab", "abcd"]) == ["AB", "ABCD"]
+    assert upper_case(["aaa"]) == ["AAA"]
+
+
+def test_getElement():
+    assert get_element([0, 1], 1) == 0
+    assert get_element([3, 79, 55, 3], 2) == 55
+    assert get_element([3, 79, 55], 2) == 55
+    assert get_element([3, 79], 2) == -1
+
+
+def test_get_number_of_nodes():
+    root = TreeNode()
+    assert get_number_of_nodes(root) == 1
+
+    root = TreeNode()
+    root.add_left(TreeNode())
+    assert get_number_of_nodes(root) == 2
+
+    root = TreeNode()
+    root.add_left(TreeNode())
+    root.add_right(TreeNode())
+    assert get_number_of_nodes(root) == 3
+
+    root = TreeNode()
+    root.add_left(TreeNode())
+    root.add_right(TreeNode())
+    root.left.add_left(TreeNode())
+    assert get_number_of_nodes(root) == 4
+
+
+def test_count():
+    head = ListNode()
+    head.add_next(ListNode())
+    assert count(head) == 2
+
+    head = ListNode()
+    head.add_next(ListNode())
+    head.next.add_next(ListNode())
+    head.next.next.add_next(ListNode())
+    assert count(head) == 4
+
+    head = ListNode()
+    assert count(head) == 1
+
+
+def test_count_leaf_nodes():
+    root = TreeNode()
+    assert count_leaf_nodes(root) == 1
+
+    root = TreeNode()
+    root.add_left(TreeNode())
+    assert count_leaf_nodes(root) == 1
+
+    root = TreeNode()
+    root.add_left(TreeNode())
+    root.add_right(TreeNode())
+    assert count_leaf_nodes(root) == 2
+
+    root = TreeNode()
+    root.add_left(TreeNode())
+    root.add_right(TreeNode())
+    root.left.add_left(TreeNode())
+    assert count_leaf_nodes(root) == 2
+
+
+def test_depth_binary_tree():
+    root = TreeNode()
+    assert depth_binary_tree(root) == 1
+
+    root = TreeNode()
+    root.add_left(TreeNode())
+    assert depth_binary_tree(root) == 2
+
+    root = TreeNode()
+    root.add_left(TreeNode())
+    root.add_right(TreeNode())
+    assert depth_binary_tree(root) == 2
+
+    root = TreeNode()
+    root.add_left(TreeNode())
+    root.add_right(TreeNode())
+    root.left.add_left(TreeNode())
+    assert depth_binary_tree(root) == 3
+
+
+def test_find():
+    assert find([1, 3, 4]) == 3
+    assert find([-1, 4, 2, 1]) == 2
+    assert find([2, 4, 6]) == 4
